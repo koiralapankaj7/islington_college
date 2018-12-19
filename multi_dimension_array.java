37bd99f053068f12bba7627a3460e4dc71e1b528
cricpunk <koiralapankaj007@gmail.com>
2018-12-19 10:34:35 +0545

    2d array

---
+++ multi_dimension_array.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+class Example {
+
+        public static void main(String[] args) {
+
+                // Single dimension array
+                int a[] = { 1, 2, 3, 4 };
+                int b[] = { 1, 2, 3, 4, 5, 6 };
+                int c[] = { 1, 2, 3, 4, 5, 6, 7 };
+
+                // int d[] = { 1, 2, 3, 4, 5, 6, 7 };
+
+                // Multi dimension array
+                int abc[][] = { a, b, c };
+
+                // looping multi dimension array
+                for (int i = 0; i < abc.length; i++) {
+                        // i = 0
+                        // abc[0] => a
+                        // a => { 1, 2, 3, 4 }
+
+                        // We get single dimension array from above loop
+                        // That is why we are using second loop
+                        // This loop will print all values from single dimension array
+                        for (int j = 0; j < abc[i].length; j++) {
+                                /*
+                                 * abc[i] means value from i index of multi dimension array. which will be
+                                 * single dimension array. abc[i][j] means value of i index of multi dimension
+                                 * array followed by value of j index of single dimension array
+                                 */
+
+                                System.out.print(abc[i][j]);
+
+                        }
+                        System.out.println();
+                }
+
+        }
+}
