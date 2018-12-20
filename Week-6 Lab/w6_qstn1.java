ddd5b9cf39b8b70108ad3e092991d0a88b84caae
koiralapankajgit <koiralapankaj007@gmail.com>
2018-12-20 14:29:41 +0545

    Week-6 Lab

---
+++ Week-6 Lab/w6_qstn1.java
@@ -0,0 +1,16 @@
+// Q.1. Write a java program to show the implementation of parameterize
+// constructor and methods.
+// step 1: Open bluej.
+// 2: Create a new project "week6".
+// 3: Create a class OnlineShop.
+// 4: declare itemname, price, and quantity as class level variable.
+// 5: wirte the parameterize constructor then initialize itemname, price and
+// quantity.
+// 6: define a method to display the details of the item.
+// 8: define a method to sell the registered item.
+// a. name of the method is itemSell.
+// b. ask the value for quantitiy to purchase as parameter.
+// c. if item has not been sold out, sell the item then calculate the total
+// amount. Reduce the quantity in inventory also.
+// d. If customer tries to purchase the sold item then display the message, item
+// is already sold.
