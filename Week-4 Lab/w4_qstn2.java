5d06a5849dc387d0c87e509622f166fd1c9e5194
koiralapankajgit <koiralapankaj007@gmail.com>
2018-12-20 09:42:45 +0545

    Lab 4

---
+++ Week-4 Lab/w4_qstn2.java
@@ -0,0 +1,62 @@
+// 2. Take input of age of 3 people by user and determine oldest and
+// youngest among them.
+
+class Oldest {
+
+        public static void main(String[] args) {
+
+                int ram = 20;
+                int sita = 30;
+                int hari = 25;
+
+                if (ram > sita && ram > hari) {
+                        System.out.println("Ram is oldest");
+                } else if (sita > ram && sita > hari) {
+                        System.out.println("Sita is oldest");
+                } else {
+                        System.out.println("Hari is oldest");
+                }
+
+                // There are several way to obtain this result
+                // I am doing this way so that you could learn more about if else
+                // statement
+
+                // Way 1
+                if (ram < sita && ram < hari) {
+                        System.out.println("Ram is youngest");
+                } else if (sita < ram && sita < hari) {
+                        System.out.println("Sita is youngest");
+                } else {
+                        System.out.println("Hari is youngest");
+                }
+
+                // way 2
+                // This solution seems confusing but wou can learn how we can nested if else
+                // statement
+                // This solution is for learning purpose
+                // Best and clean solution will be above one
+                if (ram > sita && ram > hari) {
+                        System.out.println("Ram is oldest");
+                        if (sita < ram) {
+                                System.out.println("Sita is youngest");
+                        } else {
+                                System.out.println("Hari is youngest");
+                        }
+                } else if (sita > ram && sita > hari) {
+                        System.out.println("Sita is oldest");
+                        if (ram < hari) {
+                                System.out.println("Ram is youngest");
+                        } else {
+                                System.out.println("Hari is youngest");
+                        }
+                } else {
+                        System.out.println("Hari is oldest");
+                        if (ram < sita) {
+                                System.out.println("Ram is youngest");
+                        } else {
+                                System.out.println("Sita is youngest");
+                        }
+                }
+
+        }
+}
