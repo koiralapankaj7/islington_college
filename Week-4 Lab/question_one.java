3aa27d0b596e5915bcd240a1ce2fc9fb2955dcad
koiralapankajgit <koiralapankaj007@gmail.com>
2018-12-20 09:27:05 +0545

    Lab 4

--- Week-4 Lab/question_one.java
+++ Week-4 Lab/question_one.java
@@ -1,3 +1,19 @@
 // 1.Write Java program to input the age then the program will show
 // if the person is eligible to vote. A person who is eligible to vote
 // must be older than or equal to 18 years old.
+
+class Vote {
+
+        public static void main(String[] args) {
+
+                // This is local variable 
+                int personAge = 20;
+         
+                 if (personAge >= 18) {
+                        System.out.println("Eligible for vote.");
+                } else {
+                        System.out.println("Not eligible for vote.");
+                }
+        }
+
+}
--- Week-4 Lab/question_two.java
+++ Week-4 Lab/question_two.java
@@ -1,2 +1,21 @@
 // 2. Take input of age of 3 people by user and determine oldest and
 // youngest among them.
+
+class Oldest {
+
+        public static void main(String[] args) {
+
+                int ram = 20;
+                int sita = 30;
+                int hari = 25;
+
+                if (ram > sita && ram > hari) {
+                        System.out.println("Ram is oldest");
+                } else if (sita > ram && sita > hari){
+                        System.out.println("Sita is oldest");
+                } else {
+                        System.out.println("Hari is oldest");
+                }
+
+        }
+}
