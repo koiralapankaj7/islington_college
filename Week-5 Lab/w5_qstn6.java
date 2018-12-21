4ed2c213ff0c4d8ebfaa807b7a956032042e93c8
koiralapankajgit <koiralapankaj007@gmail.com>
2018-12-21 10:49:18 +0545

    Week-5 Lab

--- Week-5 Lab/w5_qstn6.java
+++ Week-5 Lab/w5_qstn6.java
@@ -4,9 +4,6 @@
 
 import java.util.Scanner;
 
-// Q5. Write a program to find the sum of even digits of a given input number.
-// ex: n=23468, 2+4+6+8=20
-
 class Reverse {
 
         public static void main(String[] args) {
