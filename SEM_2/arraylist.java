e48836d909c3ed37dde1f7439685c6e19e6aa748
cricpunk <koiralapankaj007@gmail.com>
2019-03-28 07:55:38 +0545

    Array List and Exception Handling

---
+++ SEM_2/arraylist.java
@@ -0,0 +1,144 @@
+import java.util.ArrayList;
+
+class ArrayListPractice {
+
+        // The ArrayList class is a resizable array, which can be found in the java.util
+        // package.
+        // The difference between a built-in array and an ArrayList in Java, is that the
+        // size of an array cannot be modified (if you want to add or remove elements
+        // to/from an array, you have to create a new one). While elements can be added
+        // and removed from an ArrayList whenever you want. The syntax is also slightly
+        // different:
+
+        public static void main(String[] args) {
+                new ArrayListPractice().runApp();
+        }
+
+        void runApp() {
+
+                // Example
+                // Create an ArrayList object called cars that will store strings:
+
+                ArrayList<String> cars = new ArrayList<String>(); // Create an ArrayList object
+                cars.add("Volvo");
+                cars.add("BMW");
+                cars.add("Ford");
+                cars.add("Mazda");
+                System.out.println(cars);
+
+                // To access an element in the ArrayList, use the get() method and refer to the
+                // index number:
+                cars.get(0);
+                // To modify an element, use the set() method and refer to the index number:
+                cars.set(0, "Opel");
+                // To remove an element, use the remove() method and refer to the index number:
+                cars.remove(0);
+                // To remove all the elements in the ArrayList, use the clear() method:
+                cars.clear();
+                // To find out how many elements an ArrayList have, use the size method:
+                cars.size();
+
+                // Loop through the elements of an ArrayList with a for loop, and use the size()
+                // method to specify how many times the loop should run:
+                for (int i = 0; i < cars.size(); i++) {
+                        System.out.println(cars.get(i));
+                }
+
+                // You can also loop through an ArrayList with the for-each loop:
+                for (String i : cars) {
+                        System.out.println(i);
+                }
+
+                // You can also loop through an ArrayList with the Iterator
+                Iterator itr = cars.iterator();
+                while (itr.hasNext()) {
+                        System.out.println(itr.next());
+                }
+
+                // Another useful class in the java.util package is the Collections
+                // class, which include the sort() method for sorting lists alphabetically or
+                // numerically:
+                Collections.sort(cars); // Sort cars
+                for (String i : cars) {
+                        System.out.println(i);
+                }
+
+                ArrayList arrayList = new ArrayList<String>();
+
+                // rangeCheckForAdd error at runtime
+                // arrayList.add(10, 20);
+
+                // Object cannot be converted to int
+                String val = arrayList.get(0);
+
+                System.out.println(arrayList.size());
+        }
+
+        void constructorOfArrayList() {
+                // Constructors of Java ArrayList
+                // Constructor : Description
+                // ArrayList() : It is used to build an empty array list.
+                // ArrayList(Collection<? extends E> c) : It is used to build an array list that
+                // is initialized with the elements of the collection c.
+                // ArrayList(int capacity) : It is used to build an array list that has the
+                // specified initial capacity.
+        }
+
+        void methodsOfArrayList() {
+                // Method : Description
+                // void add(int index, E element) : It is used to insert the specified element
+                // at the specified position in a list.
+                // boolean add(E e) : It is used to append the specified element at the end of a
+                // list.
+                // boolean addAll(Collection<? extends E> c) : It is used to append all of the
+                // elements in the specified collection to the end of this list, in the order
+                // that they are returned by the specified collection's iterator.
+                // boolean addAll(int index, Collection<? extends E> c) : It is used to append
+                // all the elements in the specified collection, starting at the specified
+                // position of the list.
+                // void clear() : It is used to remove all of the elements from this list.
+                // void ensureCapacity(int requiredCapacity) : It is used to enhance the
+                // capacity of an ArrayList instance.
+                // E get(int index) : It is used to fetch the element from the particular
+                // position of the list.
+                // boolean isEmpty() : It returns true if the list is empty, otherwise false.
+                // int lastIndexOf(Object o) : It is used to return the index in this list of
+                // the last occurrence of the specified element, or -1 if the list does not
+                // contain this element.
+                // Object[] toArray() : It is used to return an array containing all of the
+                // elements in this list in the correct order.
+                // <T> T[] toArray(T[] a) : It is used to return an array containing all of the
+                // elements in this list in the correct order.
+                // Object clone() : It is used to return a shallow copy of an ArrayList.
+                // boolean contains(Object o) : It returns true if the list contains the
+                // specified element
+                // int indexOf(Object o) : It is used to return the index in this list of the
+                // first occurrence of the specified element, or -1 if the List does not contain
+                // this element.
+                // E remove(int index) : It is used to remove the element present at the
+                // specified position in the list.
+                // boolean remove(Object o) : It is used to remove the first occurrence of the
+                // specified element.
+                // boolean removeAll(Collection<?> c) : It is used to remove all the elements
+                // from the list.
+                // boolean removeIf(Predicate<? super E> filter) : It is used to remove all the
+                // elements from the list that satisfies the given predicate.
+                // protected void removeRange(int fromIndex, int toIndex) : It is used to remove
+                // all the elements lies within the given range.
+                // void replaceAll(UnaryOperator<E> operator) : It is used to replace all the
+                // elements from the list with the specified element.
+                // void retainAll(Collection<?> c) : It is used to retain all the elements in
+                // the list that are present in the specified collection.
+                // E set(int index, E element) : It is used to replace the specified element in
+                // the list, present at the specified position.
+                // void sort(Comparator<? super E> c) : It is used to sort the elements of the
+                // list on the basis of specified comparator.
+                // Spliterator<E> spliterator() : It is used to create spliterator over the
+                // elements in a list.
+                // List<E> subList(int fromIndex, int toIndex) : It is used to fetch all the
+                // elements lies within the given range.
+                // int size() : It is used to return the number of elements present in the list.
+                // void trimToSize() : It is used to trim the capacity of this ArrayList
+                // instance to be the list's current size.
+        }
+}
---
+++ SEM_2/exceptionhandling.java
@@ -0,0 +1,89 @@
+class EsceptionHandlingPractice {
+
+        // The Exception Handling in Java is one of the powerful mechanism to handle the
+        // runtime errors so that normal flow of the application can be maintained.
+
+        /*
+         * 1) Checked Exception: The classes which directly inherit Throwable class
+         * except RuntimeException and Error are known as checked exceptions e.g.
+         * IOException, SQLException etc. Checked exceptions are checked at
+         * compile-time.
+         * 
+         * 
+         * 2) Unchecked Exception The classes which inherit RuntimeException are known
+         * as unchecked exceptions e.g. ArithmeticException, NullPointerException,
+         * ArrayIndexOutOfBoundsException etc. Unchecked exceptions are not checked at
+         * compile-time, but they are checked at runtime.
+         * 
+         * 3) Error Error is irrecoverable e.g. OutOfMemoryError, VirtualMachineError,
+         * AssertionError etc.
+         * 
+         * 
+         * 
+         * try ==>>> The "try" keyword is used to specify a block where we should place
+         * exception code. The try block must be followed by either catch or finally. It
+         * means, we can't use try block alone.
+         * 
+         * 
+         * catch ==>>> The "catch" block is used to handle the exception. It must be
+         * preceded by try block which means we can't use catch block alone. It can be
+         * followed by finally block later.
+         * 
+         * 
+         * finally ==>>> The "finally" block is used to execute the important code of
+         * the program. It is executed whether an exception is handled or not.
+         * 
+         * 
+         * throw ==>>> The "throw" keyword is used to throw an exception.
+         * 
+         * 
+         * throws ==>>> The "throws" keyword is used to declare exceptions. It doesn't
+         * throw an exception. It specifies that there may occur an exception in the
+         * method. It is always used with method signature.
+         */
+
+        public static void main(String[] args) {
+                new EsceptionHandlingPractice().runApp();
+        }
+
+        void runApp() {
+                try {
+                        // code that may raise exception
+                        int data = 100 / 0;
+                } catch (ArithmeticException e) {
+                        System.out.println(e);
+                }
+                // rest code of the program
+                System.out.println("rest of the code...");
+
+                /**
+                 * Result ==> Exception in thread main java.lang.ArithmeticException:/ by zero
+                 * rest of the code...
+                 */
+
+                // Common Scenarios of Java Exceptions
+                // ======================================================================
+                // 1) A scenario where ArithmeticException occurs
+                // If we divide any number by zero, there occurs an ArithmeticException.
+                int a = 50 / 0;// ArithmeticException
+
+                // 2) A scenario where NullPointerException occurs
+                // If we have a null value in any variable, performing any operation on the
+                // variable throws a NullPointerException.
+                String s = null;
+                System.out.println(s.length());// NullPointerException
+
+                // 3) A scenario where NumberFormatException occurs
+                // The wrong formatting of any value may occur NumberFormatException. Suppose I
+                // have a string variable that has characters, converting this variable into
+                // digit will occur NumberFormatException.
+                String s1 = "abc";
+                int i = Integer.parseInt(s1);// NumberFormatException
+
+                // 4) A scenario where ArrayIndexOutOfBoundsException occurs
+                // If you are inserting any value in the wrong index, it would result in
+                // ArrayIndexOutOfBoundsException as shown below:
+                int arr[] = new int[5];
+                arr[10] = 50; // ArrayIndexOutOfBoundsException
+        }
+}
